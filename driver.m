58abf9515208f14f92bdfaa55cdd77e0ad3fd5c8
AndreaScarpellini1 <91903540+AndreaScarpellini1@users.noreply.github.com>
2023-04-11 19:41:20 +0200

    starting commit

---
+++ driver.m
@@ -0,0 +1,79 @@
+%
+% population driver for ORd model to create stable population and
+% keep conductances, biomarkers and state variables
+%
+%%
+clear all;
+clc;
+close all;
+
+%% Fixed values 
+BCL = 800;  %BCL in ms 
+Nstim=500;  %Number of stimulus
+
+%% Uploading the gAmps and the drug effects 
+
+%gAmps = [ICaL ITo INaCa IK1 IKr IKs INaf INaL INaK];
+%gAmps = random('unif',0.70,1.30,[1000,9]);
+%save('gAmps.mat','gAmps','-v7.3');
+
+%gAmps matrix [10 different models X 9 different ionic chanels]
+load mask.mat
+
+ngAmps = length(gAmps(1,:));   % ngAmps = 9
+nsamples = length(gAmps(:,1)); % nsamples = 10
+
+%% Ritonavir --> modifies the following channels:
+%Gna
+%GNaL
+%GKr
+%GCaL
+%Ritonavir Terapeutic value 
+EFTPC= 0.4369; %[μM]
+
+%%
+moltiplicative_coeffiecients=[1.0, 2.0, 10, 100];
+name_models={'M_1','M_2','M_3','M_4','M_5','M_6','M_7','M_8','M_9','M_10'};
+name_molt_coeff={'1','2','10','100'};
+index=0;
+
+for k =moltiplicative_coeffiecients
+    index=index+1;
+    %Concentration= k * terapeutic value 
+    IC= k* EFTPC;
+    
+    %Blocking factor for Na, NaL, Kr, CaL
+    BFNa=1.0/(1+(IC/27.96163));
+    BFNaL=1.0/(1+(IC/7.175));
+    BFKr=1.0/(1+(IC/5.157));
+    BFCaL=1.0/(1+(IC/8.228)^1.3);
+    
+    
+  
+    tstart=tic;
+    cont=0;
+    settings.minIsqIni=Nstim*BCL/(1000*60);
+    settings.BCL=BCL;
+    for ii=1:nsamples
+    
+        %Changing the value of the gAmps according to the drug effect. 
+        gAmps_Drug=gAmps(ii,:);
+        gAmps_Drug(7)=gAmps_Drug(7)*BFNa;  %Na
+        gAmps_Drug(8)=gAmps_Drug(7)*BFNaL; %NaL
+        gAmps_Drug(5)=gAmps_Drug(5)*BFKr;  %Kr
+        gAmps_Drug(1)=gAmps_Drug(1)*BFCaL; %CaL
+    
+        [t,y,currents]=main2019_ORd_MMChA(gAmps_Drug,settings,[],[],1);
+        %t --> time, resolution of 24003
+        %y(1) --> transmembrane potential 
+    
+        save(['ORm_Output/',name_models{ii},'_',name_molt_coeff{index},'xEFTPC.mat'],'t','y','currents');
+        fprintf([name_models{ii},'of coeff ',name_molt_coeff{index},' completed!']);
+        telapsed = toc(tstart);
+        fprintf('-Execution time: %f\n',telapsed);
+        fprintf('############################################################\n')
+       
+    end
+    
+   
+end 
---
+++ plot_APs.m
@@ -0,0 +1,64 @@
+clc
+close all 
+clear 
+%% AP without drugs 
+
+load mask.mat
+
+%% Import output 
+list = dir('ORm_Output');
+list(1)=[];
+list(1)=[];
+
+%% Separating the models 
+
+for index=1:4
+    M1(index) =load(['ORm_Output\',list(index).name]);
+end
+%%
+for index=9:12
+    M2(index) =load(['ORm_Output\',list(index).name]);
+end
+%%
+for index=13:16
+    M3(index) =load(['ORm_Output\',list(index).name]);
+end
+for index=17:20
+    M4(index) =load(['ORm_Output\',list(index).name]);
+end
+for index=21:24
+    M5(index) =load(['ORm_Output\',list(index).name]);
+end
+for index=25:28
+    M6(index) =load(['ORm_Output\',list(index).name]);
+end
+for index=29:32
+    M7(index) =load(['ORm_Output\',list(index).name]);
+end
+for index=33:36
+    M8(index) =load(['ORm_Output\',list(index).name]);
+end
+for index=37:40
+    M9(index) =load(['ORm_Output\',list(index).name]);
+end
+
+%%
+BCL = 800;  %BCL in ms 
+Nstim=500;  %Number of stimulus
+cont=0;
+settings.minIsqIni=Nstim*BCL/(1000*60);
+settings.BCL=BCL;
+[t,y,currents]=main2019_ORd_MMChA(gAmps(1,:),settings,[],[],1);
+%%
+plot(M1(1).t,M1(1).y(:,1),LineWidth=1)
+title("Model 1")
+grid on 
+hold on 
+box on 
+plot(M1(1).t,M1(2).y(:,1),LineWidth=1)
+plot(M1(1).t,M1(3).y(:,1),LineWidth=1)
+plot(M1(1).t,M1(4).y(:,1),LineWidth=1)
+plot(t,y(:,1),LineWidth=1)
+legend('1','2','10','100','Normal');
+
+
