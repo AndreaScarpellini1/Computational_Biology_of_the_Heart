42d6c8403a8d94e9d3b73dd6a5175efd83154858
AndreaScarpellini1 <91903540+AndreaScarpellini1@users.noreply.github.com>
2023-04-12 16:20:51 +0200

    Update plot_APs.m

--- plot_APs.m
+++ plot_APs.m
@@ -2,53 +2,51 @@ clc
 close all 
 clear 
 %% AP without drugs 
-
 load mask.mat
-
 %% Import output 
 list = dir('ORm_Output');
 list(1)=[];
 list(1)=[];
-
 %% Separating the models 
-
-for index=1:4
-    M1(index) =load(['ORm_Output\',list(index).name]);
-end
-%%
-for index=9:12
-    M2(index) =load(['ORm_Output\',list(index).name]);
-end
-%%
-for index=13:16
-    M3(index) =load(['ORm_Output\',list(index).name]);
-end
-for index=17:20
-    M4(index) =load(['ORm_Output\',list(index).name]);
-end
-for index=21:24
-    M5(index) =load(['ORm_Output\',list(index).name]);
-end
-for index=25:28
-    M6(index) =load(['ORm_Output\',list(index).name]);
-end
-for index=29:32
-    M7(index) =load(['ORm_Output\',list(index).name]);
-end
-for index=33:36
-    M8(index) =load(['ORm_Output\',list(index).name]);
-end
-for index=37:40
-    M9(index) =load(['ORm_Output\',list(index).name]);
+for index=1:5
+    M10(index) =load(['ORm_Output\',list(index).name]);
 end
 
-%%
-BCL = 800;  %BCL in ms 
-Nstim=500;  %Number of stimulus
-cont=0;
-settings.minIsqIni=Nstim*BCL/(1000*60);
-settings.BCL=BCL;
-[t,y,currents]=main2019_ORd_MMChA(gAmps(1,:),settings,[],[],1);
+for index=6:10
+    M1(index-5) =load(['ORm_Output\',list(index).name]);
+end
+
+for index=11:15
+    M2(index-10) =load(['ORm_Output\',list(index).name]);
+end
+for index=16:20
+    M3(index-15) =load(['ORm_Output\',list(index).name]);
+end
+for index=21:25
+    M4(index-20) =load(['ORm_Output\',list(index).name]);
+end
+for index=26:30
+    M5(index-25) =load(['ORm_Output\',list(index).name]);
+end
+for index=31:35
+    M6(index-30) =load(['ORm_Output\',list(index).name]);
+end
+for index=36:40
+    M7(index-35) =load(['ORm_Output\',list(index).name]);
+end
+for index=41:45
+    M8(index-40) =load(['ORm_Output\',list(index).name]);
+end
+for index=46:50
+    M9(index-45) =load(['ORm_Output\',list(index).name]);
+end
+
+%% PLOT NO DRUG ACTION POTENTIALS 
+M_list={M1,M2,M3,M4,M5,M6,M7,M8,M9,M10};
+for  i=1:1:10
+
+end 
+
 %% PLOT DIFFERENT MODELS
 title_name={'Model 1','Model 2','Model 3','Model 4','Moel 5',...
             'Model 6','Model 7','Model 8','Model 9','Moel 10'};
