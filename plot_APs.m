d13079e9ad0ba70393ecc458f23da8a9af8b30c1
AndreaScarpellini1 <91903540+AndreaScarpellini1@users.noreply.github.com>
2023-04-12 16:50:11 +0200

    Update plot_APs.m

--- plot_APs.m
+++ plot_APs.m
@@ -23,17 +23,3 @@ for  i=5:5:50
     title("no drug influenced APs")
 end 
 
-%% PLOT DIFFERENT MODELS
-title_name={'Model 1','Model 2','Model 3','Model 4','Moel 5',...
-            'Model 6','Model 7','Model 8','Model 9','Moel 10'};
-
-plot(M1(1).t,M1(1).y(:,1),LineWidth=1)
-title("Model 1")
-grid on 
-hold on 
-box on 
-plot(M1(1).t,M1(2).y(:,1),LineWidth=1)
-plot(M1(1).t,M1(3).y(:,1),LineWidth=1)
-plot(M1(1).t,M1(4).y(:,1),LineWidth=1)
-plot(t,y(:,1),LineWidth=1)
-legend('1','2','10','100','Normal');
