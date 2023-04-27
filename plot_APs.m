6150a5e95ff398c1d6370d6546785c396e4b0043
AndreaScarpellini1 <91903540+AndreaScarpellini1@users.noreply.github.com>
2023-04-27 06:45:26 +0200

    Update plot_APs.m

--- plot_APs.m
+++ plot_APs.m
@@ -14,6 +14,7 @@ for i=1:50
     M_list(i).('name')=list(i).name;
 end
 %% PLOT NO DRUG ACTION POTENTIALS 
+figure(100)
 for  i=5:5:50
     plot(M_list(i).t,M_list(i).y(:,1))
     hold on
@@ -21,18 +22,21 @@ for  i=5:5:50
     box  on 
     title("No Drug Influenced APs")
 end 
+saveas(figure(100), 'Plots/No_drug.jpg')
 %%
 s_name={'Sub 1','Sub 2','Sub 3','Sub 4','Sub 5','Sub 6','Sub 7','Sub 8','Sub 9','Sub 10'};
 index=0;
 for i=1:5:46 
     index=index+1;
-    figure()
+    figure(index)
     for j=0:1:4
         plot(M_list(i+j).t,M_list(i+j).y(:,1),linewidth=1);
         hold on 
+        grid on 
         title([s_name(index)])
     end
     legend("100x","10x","1x","2x","N")
+    saveas(figure(index), ['Plots/',[s_name{index}],'.jpg'])
 end 
 
 
