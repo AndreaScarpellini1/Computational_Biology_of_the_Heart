ad2cef7a4a7f88f4905611569c12f30253659ede
AndreaScarpellini1 <91903540+AndreaScarpellini1@users.noreply.github.com>
2023-04-28 04:59:12 +0200

    Update measure_apd90.m

--- measure_apd90.m
+++ measure_apd90.m
@@ -2,40 +2,67 @@ function t=measure_apd90(ap,o)
 %The ADP90 is a measurement of the action potential duration at 90%
 %repolarization.
 
-%Input: ap:Action Pontential vector (length(24003)
+%Input: ap.y
 %       o :Option if you want to plot ==1
 %Output: ADP90
-
-    [y1,t1]=max(ap(1:8001));
-    [y2,t2]=max(ap(8001:16002));
-    [y3,t3]=max(ap(16003:24003));
     
-    M_indexes=[t1,t2+8001,t3+16003];
-    M_values=[y1,y2,y3];
+    % Value and index of the max point
+    [y1,t1]=max(ap.y(1:8001,1));
+    [y2,t2]=max(ap.y(8002:16002,1));
+    [y3,t3]=max(ap.y(16003:24003,1));
+    
+    M_indexes=[t1,t2+8001,t3+16002];
+    M_values= [y1,y2,y3];
 
-
-    rest_pot=-87.5;
+    % As V-rest I consider the first point of the AP 
+    rest_pot=[ap.y(1,1),ap.y(8002,1),ap.y(16003,1)]; 
+    
+    % Theoretical values of the APD90 
     N_values=M_values-abs(M_values-rest_pot)*0.90;
-    [y1,t1]=min(abs(ap(1    : 8001)-N_values(1)));
-    [y2,t2]=min(abs(ap(8001 :16002)-N_values(2)));
-    [y3,t3]=min(abs(ap(16003:24003)-N_values(3)));
-    N_indexes=[t1,t2+8001,t3+16003];
 
-    t=abs(M_indexes-N_indexes)*0.1000;
+    % The most similar value (msv) in the vector (*but is it higher or lower?)
+    [y1,t1]=min(abs(ap.y(1:8001,1) - N_values(1)));
+    [y2,t2]=min(abs(ap.y(8002:16002,1)- N_values(2)));
+    [y3,t3]=min(abs(ap.y(16003:24003,1)- N_values(3)));
+
+    %Find the index of the msv in the vector
+    N_indexes=[t1,t2+8001,t3+16002];
+
+    %Values of the difference  
+    N_values=[y1,y2,y3];
+    
+    % Interval of time between the first time point and the msv  time point
+   
+    t=[abs(ap.t(1)-ap.t(N_indexes(1)));
+       abs(ap.t(8002)-ap.t(N_indexes(2)));
+       abs(ap.t(16003)-ap.t(N_indexes(3)))];
 
     
     if (o==1)
         figure()
-        plot(ap)
-        title("AP with max and 0.9 points")
+        plot(ap.t,ap.y(:,1))
+        title("ap.y(:,1) APD90")
         hold on 
         grid on
-        plot(M_indexes(1),M_values(1),'o',linewidth=1)
-        plot(M_indexes(2),M_values(2),'o',linewidth=1)
-        plot(M_indexes(3),M_values(3),'o',linewidth=1)
-        plot(N_indexes(1),N_values(1),'o',linewidth=1)
-        plot(N_indexes(2),N_values(2),'o',linewidth=1)
-        plot(N_indexes(3),N_values(3),'o',linewidth=1)
+        plot(ap.t(M_indexes(1)),ap.y(M_indexes(1),1),'o',linewidth=1)
+        plot(ap.t(M_indexes(2)),ap.y(M_indexes(2),1),'o',linewidth=1)
+        plot(ap.t(M_indexes(3)),ap.y(M_indexes(3),1),'o',linewidth=1)
+        plot(ap.t(N_indexes(1)),ap.y(N_indexes(1),1),'x',linewidth=2)
+        plot(ap.t(N_indexes(2)),ap.y(N_indexes(2),1),'x',linewidth=2)
+        plot(ap.t(N_indexes(3)),ap.y(N_indexes(3),1),'x',linewidth=2)
+        plot(ap.t(1),    rest_pot(1),'x',linewidth=2)
+        plot(ap.t(8002), rest_pot(2),'x',linewidth=2)
+        plot(ap.t(16003),rest_pot(3),'x',linewidth=2)
+        
+        vline(ap.t(8002), 'k--'); 
+        vline(ap.t(1), 'k--'); 
+        vline(ap.t(16003), 'k--'); 
+        vline(ap.t(N_indexes(1)), 'k--'); 
+        vline(ap.t(N_indexes(2)), 'k--'); 
+        vline(ap.t(N_indexes(3)), 'k--'); 
+        ylim([min(ap.y(:,1)),max(ap.y(:,1))]);
+        xlim([min(ap.t),max(ap.t)]);
+        
     end 
 end 
 
