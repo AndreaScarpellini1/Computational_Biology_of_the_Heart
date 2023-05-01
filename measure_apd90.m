2b416311f4afdd7283097ee96dc3a81d466a7c63
AndreaScarpellini1 <91903540+AndreaScarpellini1@users.noreply.github.com>
2023-05-01 19:17:20 +0200

    updating plot

--- measure_apd90.m
+++ measure_apd90.m
@@ -21,12 +21,12 @@ function t=measure_apd90(ap,o)
     N_values=M_values-abs(M_values-rest_pot)*0.90;
 
     % The most similar value (msv) in the vector (*but is it higher or lower?)
-    [y1,t1]=min(abs(ap.y(1:8001,1) - N_values(1)));
-    [y2,t2]=min(abs(ap.y(8002:16002,1)- N_values(2)));
-    [y3,t3]=min(abs(ap.y(16003:24003,1)- N_values(3)));
+    [y1,r1]=min(abs(ap.y(t1:8001,1) - N_values(1)));
+    [y2,r2]=min(abs(ap.y(t2+8001:16002,1)- N_values(2)));
+    [y3,r3]=min(abs(ap.y(t3+16002:24003,1)- N_values(3)));
 
     %Find the index of the msv in the vector
-    N_indexes=[t1,t2+8001,t3+16002];
+    N_indexes=[t1-1+r1,t2-1+8001+r2,t3-1+16002+r3];
 
     %Values of the difference  
     N_values=[y1,y2,y3];
@@ -45,15 +45,15 @@ function t=measure_apd90(ap,o)
         hold on 
         grid on
 
-        plot(ap.t(M_indexes(1)),ap.y(M_indexes(1),1),'o',linewidth==1)
-        plot(ap.t(M_indexes(2)),ap.y(M_indexes(2),1),'o',linewidth==1)
-        plot(ap.t(M_indexes(3)),ap.y(M_indexes(3),1),'o',linewidth==1)
-        plot(ap.t(N_indexes(1)),ap.y(N_indexes(1),1),'x',linewidth==2)
-        plot(ap.t(N_indexes(2)),ap.y(N_indexes(2),1),'x',linewidth==2)
-        plot(ap.t(N_indexes(3)),ap.y(N_indexes(3),1),'x',linewidth==2)
-        plot(ap.t(1),    rest_pot(1),'x',linewidth==2)
-        plot(ap.t(8002), rest_pot(2),'x',linewidth==2)
-        plot(ap.t(16003),rest_pot(3),'x',linewidth==2)
+        plot(ap.t(M_indexes(1)),ap.y(M_indexes(1),1),'o')
+        plot(ap.t(M_indexes(2)),ap.y(M_indexes(2),1),'o')
+        plot(ap.t(M_indexes(3)),ap.y(M_indexes(3),1),'o')
+        plot(ap.t(N_indexes(1)),ap.y(N_indexes(1),1),'x')
+        plot(ap.t(N_indexes(2)),ap.y(N_indexes(2),1),'x')
+        plot(ap.t(N_indexes(3)),ap.y(N_indexes(3),1),'x')
+        plot(ap.t(1),    rest_pot(1),'x')
+        plot(ap.t(8002), rest_pot(2),'x')
+        plot(ap.t(16003),rest_pot(3),'x')
         
         vline(ap.t(8002), 'k--'); 
         vline(ap.t(1), 'k--'); 
@@ -64,14 +64,6 @@ function t=measure_apd90(ap,o)
         ylim([min(ap.y(:,1)),max(ap.y(:,1))]);
         xlim([min(ap.t),max(ap.t)]);
         
-
-        plot(M_indexes(1),M_values(1),'o',linewidth==1)
-        plot(M_indexes(2),M_values(2),'o',linewidth==1)
-        plot(M_indexes(3),M_values(3),'o',linewidth==1)
-        plot(N_indexes(1),N_values(1),'o',linewidth==1)
-        plot(N_indexes(2),N_values(2),'o',linewidth==1)
-        plot(N_indexes(3),N_values(3),'o',linewidth==1)
-
     end 
 end 
 
